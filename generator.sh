69749cc9febf080c7edf0d74b34bf0f9c8eee990
Chris David Miles <chrisdavidmiles@users.noreply.github.com>
2018-10-14 02:31:29 -0600

    Add generator.sh
    This bash script can be run as a cron to generate and update tor-redirect.js to scrape the latest IP list from torproject.org.

---
+++ generator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# DEFINE FILES
+torWorkingDir="/full/path/to/project/here/";
+torSourceFile="template.js";
+torOutputFile="tor-redirect.js";
+ProductionDir="/var/www/website/path/here/";
+
+# GET INFO FROM TORPROJECT.ORG
+torLastUpdate=`curl --silent https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1 |grep 'This file was generated on' |sed 's/\# This file was generated on //g;s/ \#//g'`;
+torExitIPList=`curl --silent https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1 |grep -v "#" |sort -u |awk '{print "\"" $1 "\","}' |tr -d "\n" |sed 's/.$//'`;
+
+# COMPILE OUTPUT FILE
+sed 's/\#DATE\#/'"$torLastUpdate"'/g;s/\#ARRAY-OF-TOR-IPS\#/'"$torExitIPList"'/g' $torWorkingDir$torSourceFile > $torWorkingDir$torOutputFile;
+
+# COPY TO PRODUCTION SITE FOLDER
+rsync -avz $torWorkingDir$torOutputFile $ProductionDir$torOutputFile;
