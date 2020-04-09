dce22a49ed362983b0d22d1d361bb84891342e4b
Mario Michel <1108mario@gmail.com>
2020-04-09 11:03:46 +0200

    Tor Exit IP list moved and $(...) notation
    The Tor Exit IP list has moved.
    Use $(...) notation instead of backticked `...`

--- generator.sh
+++ generator.sh
@@ -7,8 +7,8 @@ torOutputFile="tor-redirect.js";
 ProductionDir="/var/www/website/path/here/";
 
 # GET INFO FROM TORPROJECT.ORG
-torLastUpdate=`curl --silent https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1 |grep 'This file was generated on' |sed 's/\# This file was generated on //g;s/ \#//g'`;
-torExitIPList=`curl --silent https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1 |grep -v "#" |sort -u |awk '{print "\"" $1 "\","}' |tr -d "\n" |sed 's/.$//'`;
+torLastUpdate=$(curl --silent https://check.torproject.org/torbulkexitlist?ip=1.1.1.1 | grep 'This file was generated on' | sed 's/\# This file was generated on //g;s/ \#//g')
+torExitIPList=$(curl --silent https://check.torproject.org/torbulkexitlist?ip=1.1.1.1 | grep -v "#" | sort -u | awk '{print "\"" $1 "\","}' | tr -d "\n" | sed 's/.$//')
 
 # COMPILE OUTPUT FILE
 sed 's/\#DATE\#/'"$torLastUpdate"'/g;s/\#ARRAY-OF-TOR-IPS\#/'"$torExitIPList"'/g' $torWorkingDir$torSourceFile > $torWorkingDir$torOutputFile;
